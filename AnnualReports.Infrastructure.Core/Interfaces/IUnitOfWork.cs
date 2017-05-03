2c3b71139292add96ff69bd2f3230104a5166187
Ebram Tharwat <ebram_tharwat@outlook.com>
2017-05-04 00:33:04 +0200

    implement generic UOW

--- AnnualReports.Infrastructure.Core/Interfaces/IUnitOfWork.cs
+++ AnnualReports.Infrastructure.Core/Interfaces/IUnitOfWork.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.Entity;
 
 namespace AnnualReports.Infrastructure.Core.Interfaces
 {
-    public interface IUnitOfWork
+    public interface IUnitOfWork<TContext> where TContext : DbContext
     {
         void Commit();
     }
---
+++ AnnualReports.Infrastructure.Core/UnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using AnnualReports.Infrastructure.Core.Interfaces;
+using System.Data.Entity;
+
+namespace AnnualReports.Infrastructure.Core
+{
+    public class UnitOfWork<TContext> : IUnitOfWork<TContext> where TContext : DbContext
+    {
+        #region Fields
+
+        private TContext _dbContext;
+
+        #endregion Fields
+
+        #region Ctor
+
+        public UnitOfWork(TContext context)
+        {
+            _dbContext = context;
+        }
+
+        #endregion Ctor
+
+        #region IUnitOfWork Members
+
+        public void Commit()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        #endregion IUnitOfWork Members
+    }
+}
