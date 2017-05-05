1056084f3ee034626399f461d453e8c50427e97f
Ebram Tharwat <ebram_tharwat@outlook.com>
2017-05-05 17:51:53 +0200

    add GPDynamicsFundDetails aggregate class

---
+++ AnnualReports.Domain.Core/Contracts/GPDynamicsFundDetails.cs
@@ -0,0 +1,13 @@
+﻿using AnnualReports.Domain.Core.AnnualReportsDbModels;
+
+namespace AnnualReports.Domain.Core.Contracts
+{
+    public class GPDynamicsFundDetails
+    {
+        public string Number { get; set; }
+
+        public string Description { get; set; }
+
+        public DbSource DbSource { get; set; }
+    }
+}
