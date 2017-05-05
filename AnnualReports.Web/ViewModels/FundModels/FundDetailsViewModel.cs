858fadc90a9b9b7e0146692b4d7ac42440958106
Ebram Tharwat <ebram_tharwat@outlook.com>
2017-05-06 01:01:51 +0200

    add viewmodel for funds

---
+++ AnnualReports.Web/ViewModels/FundModels/FundDetailsViewModel.cs
@@ -0,0 +1,24 @@
+﻿using AnnualReports.Domain.Core.AnnualReportsDbModels;
+using System;
+
+namespace AnnualReports.Web.ViewModels.FundModels
+{
+    public class FundDetailsViewModel
+    {
+        public int Id { get; set; }
+
+        public string FundNumber { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public Int16 Year { get; set; }
+
+        public string MCAG { get; set; }
+
+        public DbSource DbSource { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public string MapTo { get; set; }
+    }
+}
