681d17647db3a5c27b6330dd4103d4810d87b9f5
Ebram Tharwat <ebram_tharwat@outlook.com>
2017-05-05 23:29:37 +0200

    add page settings class

---
+++ AnnualReports.Application.Core/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+
+namespace AnnualReports.Application.Core
+{
+    public static class AppSettings
+    {
+        public static int PageSize
+        {
+            get { return int.Parse(ConfigurationManager.AppSettings["PageSize"]); }
+        }
+    }
+}
