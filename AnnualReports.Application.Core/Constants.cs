9ebae2f4874b3442ba90b4df8774d5db5f61b978
Ebram Tharwat <ebram_tharwat@outlook.com>
2017-05-06 01:00:56 +0200

    add Constants  class

---
+++ AnnualReports.Application.Core/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace AnnualReports.Application.Core
+{
+    public static class Constants
+    {
+        public const string ExcelFilesMimeType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+    }
+}
