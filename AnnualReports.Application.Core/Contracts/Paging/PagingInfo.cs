bd7b7ca0b49bb3357f959e0233e04c64f6bca300
Ebram Tharwat <ebram_tharwat@outlook.com>
2017-05-05 23:29:59 +0200

    add paging viewmodel

---
+++ AnnualReports.Application.Core/Contracts/Paging/PagingInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace AnnualReports.Application.Core.Contracts.Paging
+{
+    public class PagingInfo
+    {
+        public int PageIndex => PageNumber - 1;
+
+        public int PageNumber { get; set; }
+
+        public int Total { get; set; }
+    }
+}
