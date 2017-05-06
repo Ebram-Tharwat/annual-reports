3dcfaaca5dde24cc4cc5bd2d3612012378e96138
Ebram Tharwat <ebram_tharwat@outlook.com>
2017-05-06 02:30:50 +0200

    add datepicker.

--- AnnualReports.Web/App_Start/BundleConfig.cs
+++ AnnualReports.Web/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace AnnualReports.Web
 {
@@ -23,9 +22,16 @@ namespace AnnualReports.Web
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/plugins").Include(
+                        "~/Scripts/moment.js",
+                        "~/Scripts/moment-with-locales.js",
+                        "~/Scripts/bootstrap-datetimepicker.js",
+                        "~/Scripts/app/common.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/bootstrap-datetimepicker.css"));
         }
     }
 }
