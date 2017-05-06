7aeae524b8b2d4a0631dd4aa2f58f65540d64fec
Ebram Tharwat <ebram_tharwat@outlook.com>
2017-05-06 02:31:20 +0200

    add funds listing page.

---
+++ AnnualReports.Web/Controllers/FundsController.cs
@@ -0,0 +1,30 @@
+﻿using AnnualReports.Application.Core.Contracts.Paging;
+using AnnualReports.Application.Core.Interfaces;
+using AnnualReports.Domain.Core.AnnualReportsDbModels;
+using AnnualReports.Web.Extensions;
+using AnnualReports.Web.ViewModels.CommonModels;
+using AnnualReports.Web.ViewModels.FundModels;
+using System.Web.Mvc;
+
+namespace AnnualReports.Web.Controllers
+{
+    public class FundsController : Controller
+    {
+        private readonly IFundService _fundService;
+
+        public FundsController(IFundService fundService)
+        {
+            _fundService = fundService;
+        }
+
+        public ActionResult Index(YearFilterViewModel filters, int page = 1)
+        {
+            var pagingInfo = new PagingInfo() { PageNumber = page };
+            var entities = _fundService.GetAllFunds((short)filters.Year.Value, DbSource.ALL, pagingInfo);
+            var viewmodel = entities.ToMappedPagedList<Fund, FundDetailsViewModel>(pagingInfo);
+
+            ViewBag.FilterViewModel = filters;
+            return View(viewmodel);
+        }
+    }
+}
---
+++ AnnualReports.Web/ViewModels/CommonModels/YearFilterViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AnnualReports.Web.ViewModels.CommonModels
+{
+    public class YearFilterViewModel
+    {
+        [Required(ErrorMessage = "Year is required.")]
+        [UIHint("YearDatePicker")]
+        [Display(Name = "Please select year")]
+        public int? Year { get; set; } = DateTime.Now.Year;
+
+        public string DateAsYear
+        {
+            get { return Year.HasValue ? Year.Value.ToString() : ""; }
+        }
+    }
+}
