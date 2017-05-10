4ddbff24b37d32c33d55f6f41ff1461a3e0fd5bb
Ebram Tharwat <ebram_tharwat@outlook.com>
2017-05-11 00:26:07 +0200

    update DI module

--- AnnualReports.Utilities/App_Start/NinjectWebCommon.cs
+++ AnnualReports.Utilities/App_Start/NinjectWebCommon.cs
@@ -91,7 +91,8 @@ namespace AnnualReports.Utilities.App_Start
             kernel.Bind<IDistDbFundRepository>().To<DistDbFundRepository>();
             kernel.Bind<IGcDbFundRepository>().To<GcDbFundRepository>();
 
-            kernel.Bind<IRepository<Domain.Core.AnnualReportsDbModels.Fund>>().To<AnnualReportsDbEfRepository<Domain.Core.AnnualReportsDbModels.Fund>>();
+            kernel.Bind<IAnnualReportsDbFundRepository>().To<AnnualReportsDbFundRepository>();
+            kernel.Bind<IRepository<Domain.Core.AnnualReportsDbModels.Bar>>().To<AnnualReportsDbEfRepository<Domain.Core.AnnualReportsDbModels.Bar>>();
 
             #endregion Repositories
 
