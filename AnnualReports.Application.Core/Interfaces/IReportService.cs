c9b918da3e7932ca261d331890a1961e1ca9ba0f
Ebram Tharwat <ebram_tharwat@outlook.com>
2017-05-25 01:02:07 +0200

    add reporting service

---
+++ AnnualReports.Application.Core/Interfaces/IReportService.cs
@@ -0,0 +1,10 @@
+﻿using AnnualReports.Application.Core.Contracts.Reports;
+using System.Collections.Generic;
+
+namespace AnnualReports.Application.Core.Interfaces
+{
+    public interface IReportService
+    {
+        List<FundsReportDataItemDetails> GetFundsReportData(int year, string fundNumber = null, string barNumber = null);
+    }
+}
