00c41e20a77e65ae504d63e22577109a3619f385
Ebram Tharwat <ebram_tharwat@outlook.com>
2017-05-25 00:59:49 +0200

    update repo to get funds report data

--- AnnualReports.Infrastructure.Core/Interfaces/IAnnualReportsDbFundRepository.cs
+++ AnnualReports.Infrastructure.Core/Interfaces/IAnnualReportsDbFundRepository.cs
@@ -1,5 +1,7 @@
 ﻿using AnnualReports.Domain.Core.AnnualReportsDbModels;
+using AnnualReports.Domain.Core.Contracts;
 using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 
 namespace AnnualReports.Infrastructure.Core.Interfaces
@@ -7,5 +9,7 @@ namespace AnnualReports.Infrastructure.Core.Interfaces
     public interface IAnnualReportsDbFundRepository : IRepository<Fund>
     {
         void Delete(Expression<Func<Fund, bool>> filter);
+
+        IEnumerable<FundsReportDataRow> GetFundsReportDataRows(int year, string fundNumber = null);
     }
 }
--- AnnualReports.Infrastructure.Core/Repositories/AnnualReportsDb/AnnualReportsDbFundRepository.cs
+++ AnnualReports.Infrastructure.Core/Repositories/AnnualReportsDb/AnnualReportsDbFundRepository.cs
@@ -1,8 +1,10 @@
 ﻿using AnnualReports.Domain.Core.AnnualReportsDbModels;
+using AnnualReports.Domain.Core.Contracts;
 using AnnualReports.Infrastructure.Core.DbContexts.AnnualReportsDb;
 using AnnualReports.Infrastructure.Core.Extensions;
 using AnnualReports.Infrastructure.Core.Interfaces;
 using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 
 namespace AnnualReports.Infrastructure.Core.Repositories.AnnualReportsDb
@@ -16,6 +18,11 @@ namespace AnnualReports.Infrastructure.Core.Repositories.AnnualReportsDb
         public void Delete(Expression<Func<Fund, bool>> filter)
         {
             _dbContext.DeleteWhere(filter);
+        }
+
+        public IEnumerable<FundsReportDataRow> GetFundsReportDataRows(int year, string fundNumber = null)
+        {
+            return _dbContext.Database.SqlQuery<FundsReportDataRow>("GetFundsReportDataPro {0},{1}", year, fundNumber);
         }
     }
 }
