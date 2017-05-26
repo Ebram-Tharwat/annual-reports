63aca906d2c0c83c8a78510ae6bc7b2bd54cbff6
Ebram Tharwat <ebram_tharwat@outlook.com>
2017-05-26 12:24:33 +0200

    add DB scripts for FundsAnnualReport

---
+++ AnnualReports.Infrastructure.Core/Migrations/Scripts/Procedures/FundsAnnualReportData/GetFundsReportDataPro_UP.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE GetFundsReportDataPro
+	@Year INT,
+	@FundNumber NVARCHAR(9)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    CREATE TABLE #ReportDate (PrimaryFundNumber NVARCHAR(9), Year SMALLINT, FundDisplayName NVARCHAR(100), View_Period SMALLINT, View_FundNumber VARCHAR(9), View_BarNumber VARCHAR(9), Debit NUMERIC(19,5), Credit NUMERIC(19,5))
+	
+	INSERT #ReportDate
+	EXEC GetGCFundsReportDataPro @Year, @FundNumber
+
+	INSERT #ReportDate
+	EXEC GetDISTFundsReportDataPro @Year, @FundNumber
+
+	SELECT * FROM #ReportDate
+END
+GO
