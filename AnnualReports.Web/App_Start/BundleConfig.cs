83ec5f06fc906094381052cd7be19f001067f582
Ebram Tharwat <ebram_tharwat@outlook.com>
2017-05-27 12:59:38 +0200

    add ng lib and required plugins

--- AnnualReports.Web/App_Start/BundleConfig.cs
+++ AnnualReports.Web/App_Start/BundleConfig.cs
@@ -13,6 +13,18 @@ namespace AnnualReports.Web
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                       // AngularJs Core modules/services/directives.
+                       "~/Scripts/angular.js",
+                       "~/Scripts/angular-sanitize.js",
+                       "~/Scripts/angular-animate.js",
+                       "~/Scripts/angular-touch.js",
+                       "~/Scripts/ng-plugins/angular-eonasdan-datetimepicker.js",
+                       "~/Scripts/ng-plugins/select.js",
+                       "~/Scripts/ng-plugins/angular-block-ui.js",
+                       "~/Scripts/ui-grid.js"
+                       ));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -26,14 +38,18 @@ namespace AnnualReports.Web
                         "~/Scripts/moment.js",
                         "~/Scripts/moment-with-locales.js",
                         "~/Scripts/bootstrap-datetimepicker.js",
-                        "~/Scripts/loadingoverlay.js",
-                        "~/Scripts/toastr.js",
+                        //"~/Scripts/loadingoverlay.js",
+                        //"~/Scripts/toastr.js",
+                        "~/Scripts/select2.js",
                         "~/Scripts/app/common.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/toastr.css",
+                      //"~/Content/toastr.css",
                       "~/Content/bootstrap-datetimepicker.css",
+                      "~/Content/select.css",
+                      "~/Content/angular-block-ui.css",
+                      "~/Content/ui-grid.css",
                       "~/Content/site.css"));
         }
     }
