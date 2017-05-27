b71c07f4cba9b9303d53a52843df3c6db2017d46
Ebram Tharwat <ebram_tharwat@outlook.com>
2017-05-27 13:01:20 +0200

    add report controller and API

---
+++ AnnualReports.Web/API/ReportsApiController.cs
@@ -0,0 +1,25 @@
+﻿using AnnualReports.Application.Core.Interfaces;
+using System.Web.Http;
+
+namespace AnnualReports.Web.API
+{
+    [RoutePrefix("api/report")]
+    public class ReportsApiController : ApiController
+    {
+        private readonly IReportService _reportService;
+        private readonly IFundService _fundService;
+
+        public ReportsApiController(IReportService reportService, IFundService fundService)
+        {
+            _reportService = reportService;
+            _fundService = fundService;
+        }
+
+        [Route("funds-annualreport/{year:int}/{fundId?}")]
+        [HttpGet]
+        public IHttpActionResult GetFundsReportData(int year, int? fundId = null)
+        {
+            return Ok(_reportService.GetFundsReportData(year, fundId));
+        }
+    }
+}
---
+++ AnnualReports.Web/Controllers/ReportsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AnnualReports.Application.Core.Services;
+
+namespace AnnualReports.Web.Controllers
+{
+    public class ReportsController : Controller
+    {
+        private readonly FundService _fundService;
+
+        public ReportsController(FundService fundService)
+        {
+            _fundService = fundService;
+        }
+
+        public ActionResult FundsAnnualReport()
+        {
+            return View();
+        }
+    }
+}
