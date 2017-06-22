8dc27dffe979a21016160f4b82068f855c0e0c4c
Ebram Tharwat <ebram_tharwat@outlook.com>
2017-06-22 21:17:24 +0200

    allow for automatic migrations in release mode.

--- AnnualReports.Infrastructure.Core/Migrations/AnnualReportsDb/201705261810353_FundsAnnualReportData.cs
+++ AnnualReports.Infrastructure.Core/Migrations/AnnualReportsDb/201705261810353_FundsAnnualReportData.cs
@@ -1,8 +1,7 @@
 namespace AnnualReports.Infrastructure.Core.Migrations.AnnualReportsDb
 {
-    using System;
     using System.Data.Entity.Migrations;
-    
+
     public partial class FundsAnnualReportData : DbMigration
     {
         public override void Up()
