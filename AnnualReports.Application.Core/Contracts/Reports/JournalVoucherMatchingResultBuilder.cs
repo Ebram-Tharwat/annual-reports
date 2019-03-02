1ecb2e28d1e84d30d7111037e093204b2b5e7cc4
Ebram Khalil <ebram_tharwat@hotmail.com>
2019-03-02 12:23:55 +0200

    Add result builder class for unmatched funds in Journal Voucher

---
+++ AnnualReports.Application.Core/Contracts/Reports/JournalVoucherMatchingResultBuilder.cs
@@ -0,0 +1,50 @@
+﻿using AnnualReports.Domain.Core.AnnualReportsDbModels;
+using System.Collections.Generic;
+
+namespace AnnualReports.Application.Core.Contracts.Reports
+{
+    public class JournalVoucherMatchingResultBuilder
+    {
+        public List<JournalVoucherUnamtchedResult> UnmatchedFunds { get; }
+
+        public JournalVoucherMatchingResultBuilder()
+        {
+            UnmatchedFunds = new List<JournalVoucherUnamtchedResult>();
+        }
+
+        public void ReportUnmatchedFund(int rowIndex, string sheetName, string unmatchedFund)
+        {
+            UnmatchedFunds.Add(new JournalVoucherUnamtchedResult(rowIndex, unmatchedFund, sheetName, "Annual Reports", null, ""));
+        }
+
+        public void ReportUnmatchedGcFund(int rowIndex, string sheetName, string unmatchedFund, JournalVoucherType journalVoucher, string unmatchedJvRuleAccount)
+        {
+            UnmatchedFunds.Add(new JournalVoucherUnamtchedResult(rowIndex, unmatchedFund, sheetName, "GC", journalVoucher, unmatchedJvRuleAccount));
+        }
+
+        public void ReportUnmatchedDistFund(int rowIndex, string sheetName, string unmatchedFund, JournalVoucherType journalVoucher, string unmatchedJvRuleAccount)
+        {
+            UnmatchedFunds.Add(new JournalVoucherUnamtchedResult(rowIndex, unmatchedFund, sheetName, "DIST", journalVoucher, unmatchedJvRuleAccount));
+        }
+
+        public class JournalVoucherUnamtchedResult
+        {
+            public JournalVoucherUnamtchedResult(int rowIndex, string fundId, string sheetName, string database, JournalVoucherType? journalVoucher, string journalVoucherRuleAccount)
+            {
+                RowIndex = rowIndex;
+                FundId = fundId;
+                SheetName = sheetName;
+                Database = database;
+                JournalVoucher = journalVoucher;
+                JournalVoucherRuleAccount = journalVoucherRuleAccount;
+            }
+
+            public int RowIndex { get; set; }
+            public string FundId { get; set; }
+            public string SheetName { get; set; }
+            public string Database { get; set; }
+            public JournalVoucherType? JournalVoucher { get; set; }
+            public string JournalVoucherRuleAccount { get; set; }
+        }
+    }
+}
